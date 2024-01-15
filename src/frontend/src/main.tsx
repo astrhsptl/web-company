ce45250c3ef213c7fd7111173b14fcdc873ff009
astrhsptl <dctrback@bk.ru>
2024-01-15 18:33:40 +0300

    add init

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from 'react-router-dom';
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
+  </BrowserRouter>,
 )
