c67ae06d3a51bd651db6cf34b9aa822e13512403
astrhsptl <dctrback@bk.ru>
2024-01-19 19:25:10 +0300

    add ...

--- src/frontend/src/pages/homepage/index.tsx
+++ src/frontend/src/pages/homepage/index.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "@/widgets/footer/Footer"
 import { HomepageContent, HomepageHead } from "@/widgets/homepage"
 export const Homepage = () => {
     return (
         <>
             <HomepageHead />
             <HomepageContent />
+            <Footer />
         </>
     )
 }
