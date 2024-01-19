0bf5c48d06c2e28feff8b2a80a543e4adeac8b15
astrhsptl <dctrback@bk.ru>
2024-01-19 10:15:48 +0300

    add some to fullpage

---
+++ src/frontend/src/widgets/homepage/HomepageContent/FeedbackRedirect.tsx
@@ -0,0 +1,14 @@
+export const FeedbackRedirect = () => {
+  return (
+    <div className="fp-redirect">
+      <div className="fp-asking">
+        Мы спрашиваем "Почему?", а не "Что?"
+      </div>
+      <div className="fp-description">
+        Для построения долгосрочной работы с клиентом мы
+        пользуемся принципом "задавай вопрос "Почему?", а не 
+        "Что?""
+      </div>
+    </div>
+  )
+}
