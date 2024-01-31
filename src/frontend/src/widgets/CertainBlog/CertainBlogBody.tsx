2aaef976d9aa53dbf8569251004fb74b528fbb1d
astrhsptl <dctrback@bk.ru>
2024-01-31 18:16:47 +0300

    add .

---
+++ src/frontend/src/widgets/CertainBlog/CertainBlogBody.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { Loader } from "@/shared/components";
+import { BlogCertainContent } from "./BlogCertainContent";
+import { usePostByIdHook } from "@/shared/api/FetchHooks/BlogFetchHooks";
+import "./styles/index.css";
+
+export const CertainBlogBody = () => {
+  const { id } = useParams();
+  const { data, isLoading } = useQuery(
+    ["blogById", id], async () => await usePostByIdHook(id),
+  );
+
+  return (
+    <div className="blog-content-container">
+    {
+      isLoading ?
+      <Loader /> :
+      <BlogCertainContent data={data} />
+    }
+    </div>
+  )
+}
