515353955da71239023b3c8c61b6334784a0d88d
astrhsptl <dctrback@bk.ru>
2024-02-20 16:06:45 +0300

    add favicon

--- src/frontend/src/widgets/homepage/HomepageContent/PrinciplesOfWork.tsx
+++ src/frontend/src/widgets/homepage/HomepageContent/PrinciplesOfWork.tsx
@@ -1,29 +1,32 @@
-import thinkPrinciple from "../images/lamp.webp";
-import envisionPrinciple from "../images/eye.webp";
 import buildPrinciple from "../images/build.webp";
+import envisionPrinciple from "../images/eye.webp";
+import thinkPrinciple from "../images/lamp.webp";
 
 export const PrinciplesOfWork = () => {
   return (
     <div className="work-principles-container">
       <div className="principle">
         <img src={thinkPrinciple} alt="" className="image-principle" />
-        <p className="title-principle">we suck with u</p>
+        <p className="title-principle">МЫ ДУМАЕМ ВМЕСТЕ С ВАМИ</p>
         <p className="description-principle">
-          we suck with u so hark, how u like
+          Инновационная стратегия Стратегия цифрового продукта Технологическая
+          архитектура Информационная архитектура
         </p>
       </div>
       <div className="principle">
         <img src={envisionPrinciple} alt="" className="image-principle" />
-        <p className="title-principle">we suck with u</p>
+        <p className="title-principle">МЫ ПРЕДСТАВЛЯЕМ ВМЕСТЕ С ВАМИ</p>
         <p className="description-principle">
-          we suck with u so hark, how u like
+          Дизайн цифрового продукта Исследования и дизайн UX Дизайн веб-сайта
+          Дизайн фирменного стиля
         </p>
       </div>
       <div className="principle principle__border-blocked">
         <img src={buildPrinciple} alt="" className="image-principle" />
-        <p className="title-principle">we suck with u</p>
+        <p className="title-principle">МЫ СОЗДАЕМ ВМЕСТЕ С ВАМИ</p>
         <p className="description-principle">
-          we suck with u so hark, how u like
+          Разработка веб-приложений Разработка Magento Разработка Wordpress
+          Разработка мобильных приложений
         </p>
       </div>
     </div>
