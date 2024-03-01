69e5132a70464827ea479061fd21dd1bff96f904
Alexander Kizimenko <dctrback@bk.ru>
2024-03-01 19:09:28 +0300

    asd

--- src/backend/server/urls.py
+++ src/backend/server/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from django.views.static import serve
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -36,6 +37,12 @@ urlpatterns = [
     ]))
 ] + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
-) + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+)     + [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
+    ]
