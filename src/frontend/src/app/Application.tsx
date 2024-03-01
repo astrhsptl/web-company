399caea98b4fb1626d91e2ced7c713720a72e271
astrhsptl <dctrback@bk.ru>
2024-03-01 16:47:28 +0300

    asd

--- src/frontend/src/app/Application.tsx
+++ src/frontend/src/app/Application.tsx
@@ -13,38 +13,48 @@ import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 export default function Application() {
-  const [complited, setComplited] = useState(false);
-  useEffect(() => {
-    if (!complited) {
-      console.log(
-        `Привет, мы команда Потребления контента и рекомендаций — создаём раздел,
-      в котором ты сейчас находишься. Хочешь присоединиться к нам?
-      Заполни форму и присоединяйся к нам: http://localhost:5173/job`
-      );
-      setComplited(true);
-    }
-  });
-  return (
-    <Routes>
-      <Route key={"*"} path={"*"} element={<NotFound />} />
-      <Route key={"/"} path={"/"} element={<Homepage />} />
-      <Route key={"/work"} path={"/work"} element={<Work />} />
-      <Route key={"/blog"} path={"/blog"} element={<Blog />} />
-      <Route key={"/service"} path={"/service"} element={<ServicePage />} />
-      <Route key={"/ticket/new"} path={"/ticket/new"} element={<Ticket />} />
-      <Route key={"/job"} path={"/job"} element={<TicketJobPage />} />
-      <Route
-        key={"/ticket/new"}
-        path={"/ticket/thanks"}
-        element={<TicketThanks />}
-      />
+    const [complited, setComplited] = useState(false);
+    useEffect(() => {
+        if (!complited) {
+            console.log(
+                `Привет, мы команда Потребления контента и рекомендаций — создаём раздел, в котором ты сейчас находишься. Хочешь присоединиться к нам? Заполни форму и присоединяйся к нам: https://https://bookshelf.labofdev.ru/job`
+            );
+            setComplited(true);
+        }
+    });
+    return (
+        <Routes>
+            <Route key={"*"} path={"*"} element={<NotFound />} />
+            <Route key={"/"} path={"/"} element={<Homepage />} />
+            <Route key={"/work"} path={"/work"} element={<Work />} />
+            <Route key={"/blog"} path={"/blog"} element={<Blog />} />
+            <Route
+                key={"/service"}
+                path={"/service"}
+                element={<ServicePage />}
+            />
+            <Route
+                key={"/ticket/new"}
+                path={"/ticket/new"}
+                element={<Ticket />}
+            />
+            <Route key={"/job"} path={"/job"} element={<TicketJobPage />} />
+            <Route
+                key={"/ticket/new"}
+                path={"/ticket/thanks"}
+                element={<TicketThanks />}
+            />
 
-      <Route key={"/blog/:id"} path={"/blog/:id"} element={<CertainBlog />} />
-      <Route
-        key={"/work/:id"}
-        path={"/work/:id"}
-        element={<CertainProduct />}
-      />
-    </Routes>
-  );
+            <Route
+                key={"/blog/:id"}
+                path={"/blog/:id"}
+                element={<CertainBlog />}
+            />
+            <Route
+                key={"/work/:id"}
+                path={"/work/:id"}
+                element={<CertainProduct />}
+            />
+        </Routes>
+    );
 }
